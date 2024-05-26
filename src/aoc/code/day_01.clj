e944d7be48bfa9cc5f6e694a08ef38da7dea9573
Yannick Lehnhausen <yannick.lehnhausen@janitza.de>
2024-05-26 12:03:00 +0200

    init

--- src/aoc/code/day_01.clj
+++ src/aoc/code/day_01.clj
@@ -1,85 +1,62 @@
 ^{:nextjournal.clerk/visibility {:code :hide :result :hide}}
-(ns aoc.code.day_01
+(ns aoc.code.day-01
   (:gen-class)
   (:require [clojure.string :as str]))
-
-;# --- Day 1: Calorie Counting ---
-
-; Santa's reindeer typically eat regular reindeer food, but they need a lot of magical energy to deliver presents on Christmas. For that, their favorite snack is a special type of star fruit that only grows deep in the jungle. The Elves have brought you on their annual expedition to the grove where the fruit grows.
-
-; To supply enough magical energy, the expedition needs to retrieve a minimum of fifty stars by December 25th. Although the Elves assure you that the grove has plenty of fruit, you decide to grab any fruit you see along the way, just in case.
-
-; Collect stars by solving puzzles. Two puzzles will be made available on each day in the Advent calendar; the second puzzle is unlocked when you complete the first. Each puzzle grants one star. Good luck!
-
-; The jungle must be too overgrown and difficult to navigate in vehicles or access from the air; the Elves' expedition traditionally goes on foot. As your boats approach land, the Elves begin taking inventory of their supplies. One important consideration is food - in particular, the number of Calories each Elf is carrying (your puzzle input).
-
-; The Elves take turns writing down the number of Calories contained by the various meals, snacks, rations, etc. that they've brought with them, one item per line. Each Elf separates their own inventory from the previous Elf's inventory (if any) by a blank line.
-
-; For example, suppose the Elves finish writing their items' Calories and end up with the following list:
-
-; ```
-; 1000
-; 2000
-; 3000
+; --- Day 1: Not Quite Lisp ---
 ;
-; 4000
-; 5000
-; 6000
+; Santa was hoping for a white Christmas, but his weather machine's "snow" function is powered by stars, and he's fresh out! To save Christmas, he needs you to collect fifty stars by December 25th.
 ;
-; 7000
-; 8000
-; 9000
+; Collect stars by helping Santa solve puzzles. Two puzzles will be made available on each day in the Advent calendar; the second puzzle is unlocked when you complete the first. Each puzzle grants one star. Good luck!
 ;
-; 10000
-;```
+; Here's an easy puzzle to warm you up.
+;
+; Santa is trying to deliver presents in a large apartment building, but he can't find the right floor - the directions he got are a little confusing. He starts on the ground floor (floor 0) and then follows the instructions one character at a time.
+;
+; An opening parenthesis, (, means he should go up one floor, and a closing parenthesis, ), means he should go down one floor.
+;
+; The apartment building is very tall, and the basement is very deep; he will never find the top or bottom floors.
+;
+; For example:
+;
+;     (()) and ()() both result in floor 0.
+;     ((( and (()(()( both result in floor 3.))))))
+;     ((((( also results in floor 3.)))))
+;     () and ( both result in floor -1 (the first basement level).)
+;      and ()() both result in floor -3.
+;
+; To what floor do the instructions take Santa?
 
-; This list represents the Calories of the food carried by five Elves:
+(defn count-paranthese [para]
+  (cond (= para "(") 1
+        (= para ")") -1
+        :else 0))
+(def input (str/split (slurp "./input/day_01/input.txt") #""))
 
-;     The first Elf is carrying food with 1000, 2000, and 3000 Calories, a total of 6000 Calories.
-;     The second Elf is carrying one food item with 4000 Calories.
-;     The third Elf is carrying food with 5000 and 6000 Calories, a total of 11000 Calories.
-;     The fourth Elf is carrying food with 7000, 8000, and 9000 Calories, a total of 24000 Calories.
-;     The fifth Elf is carrying one food item with 10000 Calories.
+(defn a [input] (map #(count-paranthese %))
+  input)
 
-; In case the Elves get hungry and need extra snacks, they need to know which Elf to ask: they'd like to know how many Calories are being carried by the Elf carrying the most Calories. In the example above, this is 24000 (carried by the fourth Elf).
-{:nextjournal.clerk/visibility {:result :hide}}
-(defn filter-empty-strings [m]
-  (->> m
-       slurp
-       str/split-lines
-       (partition-by str/blank?)
-       (map #(remove str/blank? %))))
+(defn count-parentheses [s]
+  (reduce (fn [acc ch]
+            (case ch
+              \( (inc acc)
+              \) (dec acc)
+              acc))
+          0
+          s))
 
-(defn parse-map-values->long [m] (map #(parse-long %) m))
+(defn reduce-count-parentheses [input]
+  (loop [acc 0
+         idx 0
+         coll input]
+    (if (or (empty? coll) (= acc -1))
+      (if (= acc -1) idx -1) ; return index if -1, otherwise -1 indicating not found
+      (recur (let [count (count-parentheses (first coll))]
+               (+ acc count))
+             (inc idx)
+             (rest coll)))))
 
-(defn parse-input [input]
-  (->> input
-       filter-empty-strings
-       (map #(parse-map-values->long %))
-       (map #(reduce + %))))
+(reduce + (map #(count-paranthese %)
+               input))
+(.indexOf  (map #(count-paranthese %)
+                input) -1)
 
-;### Find the Elf carrying the most Calories. How many total Calories is that Elf carrying?
-{:nextjournal.clerk/visibility {:result :show}}
-(defn solution-1 [input]
-  (->> input
-       parse-input
-       (apply max)))
-
-(solution-1 "input/day_01/input.txt")
-;# --- Part Two ---
-
-; By the time you calculate the answer to the Elves' question, they've already realized that the Elf carrying the most Calories of food might eventually run out of snacks.
-
-; To avoid this unacceptable situation, the Elves would instead like to know the total Calories carried by the top three Elves carrying the most Calories. That way, even if one of those Elves runs out of snacks, they still have two backups.
-
-; In the example above, the top three Elves are the fourth Elf (with 24000 Calories), then the third Elf (with 11000 Calories), then the fifth Elf (with 10000 Calories). The sum of the Calories carried by these three elves is 45000.
-
-;### Find the top three Elves carrying the most Calories. How many Calories are those Elves carrying in total?
-(defn solution-2 [input]
-  (->> input
-       parse-input
-       (sort >)
-       (take 3)
-       (reduce +)))
-
-(solution-2 "input/day_01/input.txt")
